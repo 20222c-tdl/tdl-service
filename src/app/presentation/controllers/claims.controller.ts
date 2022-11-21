cd057937b9c0044b91ae259bbcc198192a9b1aa6
Emiliano Prediger <epredigervianello@santandertecnologia.com.ar>
2022-11-20 23:50:47 -0300

    Autorización por rol para todos los endpoints de reclamos

--- src/app/presentation/controllers/claims.controller.ts
+++ src/app/presentation/controllers/claims.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UseGuards,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
 import { ClaimsService } from '../services/claims.service';
 import { RegisterClaimDTO } from '../../infrastructure/dtos/claims/claim-register.dto';
@@ -23,34 +14,33 @@ import { HasRoles } from '../../../auth/has-roles.decorator';
 import { RolesGuard } from '../../../auth/roles.guard';
 
 @ApiTags('Claims')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('/claims')
 export class ClaimsController {
   constructor(private readonly claimService: ClaimsService) {}
 
-  @ApiBearerAuth()
   @ApiBody({ type: RegisterClaimDTO })
   @Post()
-  @UseGuards(JwtAuthGuard, RolesGuard)
   @HasRoles(Role.USER)
   async registerClaim(@Body(ValidationPipe) newClaim: RegisterClaimDTO): Promise<Claim> {
     return this.claimService.registerClaim(newClaim);
   }
 
-  // TODO: solo un Community puede consumirlo
-  //  y el param communityId debe coincider con el sub del token
+  // TODO: el param communityId debe coincidir con el sub del token
   @ApiParam({
     name: 'communityId',
     description: 'ID necessary for getting all community claims',
     required: true,
-    type: String,
+    type: String
   })
   @Get('/community/:communityId')
+  @HasRoles(Role.COMMUNITY)
   async getClaimsByCommunity(
     @Param('communityId') communityId: string): Promise<{ claim: Claim, user: User }[]> {
     return await this.claimService.getClaimsByCommunity(new ClaimsByCommunityDTO(communityId));
   }
 
-  // TODO: ¿quien lo consume?
   @ApiParam({
     name: 'userId',
     description: 'ID necessary for getting all user claims',
@@ -58,12 +48,16 @@ export class ClaimsController {
     type: String,
   })
   @Get('/user/:userId')
+  @HasRoles(Role.USER)
   async getClaimsByUser(
     @Param('userId') userId: string): Promise<Claim[]> {
     return await this.claimService.getClaimsByUser(new ClaimsByUserDTO(userId));
   }
 
-  // TODO: user creador y community
+  /* TODO: validaciones "Nice To Have"
+   * - id de usuario que actualiza === userId del claim && newStatus === 'CLOSED'
+   * - id de community que actualiza === communityId del claim
+   */
   @ApiParam({
     name: 'claimId',
     description: 'ID necessary for updating claim status',
@@ -77,13 +71,16 @@ export class ClaimsController {
     enum: ClaimStatus,
   })
   @Patch('/:claimId/status/:status')
+  @HasRoles(Role.COMMUNITY, Role.USER)
   async updateClaimStatus(
     @Param('claimId') claimId: string,
     @Param('status') status: ClaimStatus): Promise<Claim> {
     return this.claimService.updateClaimStatus(claimId, status);
   }
 
-  // TODO: solo el user creador puede modificarlo
+  /* TODO: validaciones "Nice To Have"
+   * - id de usuario que actualiza === userId del claim
+   */
   @ApiParam({
     name: 'claimId',
     description: 'ID necessary for updating claim status',
@@ -92,6 +89,7 @@ export class ClaimsController {
   })
   @ApiBody({ type: UpdateClaimDTO })
   @Patch('/:claimId')
+  @HasRoles(Role.USER)
   async updateClaim(
     @Param('claimId') claimId: string,
     @Body(ValidationPipe) updateClaimDto: UpdateClaimDTO): Promise<Claim> {
